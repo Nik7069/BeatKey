5f793e38b832be8271622a4cfa6b09b34aaf3ec8
Nik7069 <61656334+Nik7069@users.noreply.github.com>
2025-12-22 21:33:27 +0300

    release1
    release

--- BeatKey/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_BeatKey_FILE D:/GitHub/BeatKey/BeatKey/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/BeatKey.exe)
-set(__QT_DEPLOY_TARGET_BeatKey_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_BeatKey_RUNTIME_DLLS D:/Programs/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;D:/Programs/Qt/6.10.1/mingw_64/bin/Qt6Multimedia.dll;D:/Programs/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;D:/Programs/Qt/6.10.1/mingw_64/bin/Qt6Network.dll;D:/Programs/Qt/6.10.1/mingw_64/bin/Qt6Concurrent.dll;D:/Programs/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
